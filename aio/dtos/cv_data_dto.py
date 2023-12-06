8dfdc0c4c2c39112171d50005db69ed2378596ef
Rinat1k <roma.tankin@gmail.com>
2023-12-06 14:08:11 +0100

    Created generator CV

---
+++ aio/dtos/cv_data_dto.py
@@ -0,0 +1,6 @@
+class UserDataForCVDTO:
+    def __init__(self, full_name, experience, education, languages):
+        self.full_name = full_name
+        self.experience = experience
+        self.education = education
+        self.languages = languages
